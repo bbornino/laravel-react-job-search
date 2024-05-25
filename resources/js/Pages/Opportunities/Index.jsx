3b0b357e3b2238f07bbe13dde3691514ce2e2013
Brian Bornino <brianbornino@gmail.com>
2024-05-25 07:25:03 -0700

    Initial Opportunity React Page

--- resources/js/Pages/Opportunities/Index.jsx
+++ resources/js/Pages/Opportunities/Index.jsx
@@ -1,2 +1,30 @@
 import React from 'react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import InputError from '@/Components/InputError';
+import PrimaryButton from '@/Components/PrimaryButton';
+import { useForm, Head } from '@inertiajs/react';
+
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export default function Index({ auth }) {
+    const { data, setData, post, processing, reset, errors } = useForm({
+        message: '',
+    });
+ 
+    const submit = (e) => {
+        e.preventDefault();
+        post(route('opportunities.store'), { onSuccess: () => reset() });
+    };
+ 
+    return (
+        <AuthenticatedLayout user={auth.user}>
+            <Head title="Opportunities" />
+            <Container>
+                Hello World!
+            </Container>
+        </AuthenticatedLayout>
+    );
+}
