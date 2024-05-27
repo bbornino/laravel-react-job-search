2fb3ce46cb0ad85fa7cfe441c38e1ec45f921171
Brian Bornino <brianbornino@gmail.com>
2024-05-27 15:02:08 -0700

    feature: use font awesome fonts on base datatable component

--- resources/js/Components/DataTableBase.jsx
+++ resources/js/Components/DataTableBase.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import DataTable from 'react-data-table-component';
 // import Checkbox from '@material-ui/core/Checkbox';
-
 // import ArrowDownward from '@material-ui/icons/ArrowDownward';
-
 // const sortIcon = <ArrowDownward />;
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+const sortIcon = <FontAwesomeIcon icon="fa-solid fa-arrow-down" />
+const Checkbox = <FontAwesomeIcon icon="fa-regular fa-square-check" />
+
 const selectProps = { indeterminate: isIndeterminate => isIndeterminate };
 
 function DataTableBase(props) {
 	return (
 		<DataTable
 			pagination
-			// selectableRowsComponent={Checkbox}
+			selectableRowsComponent={Checkbox}
 			selectableRowsComponentProps={selectProps}
-			// sortIcon={sortIcon}
+			sortIcon={sortIcon}
 			dense
 			{...props}
 		/>
